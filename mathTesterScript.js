46d25a53cd2b1a6388902e516f4a12964706bd3f
lizelim <limr@macstudent.com.au>
2020-08-11 22:42:54 +1000

    Added more buttons and functions to the buttons

--- mathTesterScript.js
+++ mathTesterScript.js
@@ -1,6 +1,21 @@
 var difficulty = 0;
 var score = 0;
+var highestValue = 0;
+var time = 0;
 
 function toReady(n) {
     difficulty = n
+    highestValue = 0
+}
+
+function Test() {
+    if (difficulty === 1) {
+        highestValue = 10
+    }
+    else if (difficulty === 2) {
+        highestValue = 11
+    }
+    else if (difficulty === 3) {
+        highestValue = 12
+    }
 }
