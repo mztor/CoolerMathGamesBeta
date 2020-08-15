51ad9fe8a92a5181e3f8d6a5b4a95468716b8155
jixzn <morganc1@macstudent.com.au>
2020-08-15 12:10:41 +1000

    made functions to gameplay and difficulty screen completed format on help screen

--- Unscramble/Unscramble.js
+++ Unscramble/Unscramble.js
@@ -1,10 +1,22 @@
 function toHelp() {
     document.getElementById("gameTitle").classList.add("hide");
-    document.getElementById("difficultySelect").classList.add("hide");
+    document.getElementById("gameplay").classList.add("hide");
     document.getElementById("helpMenu").classList.remove("hide")
 }
 
 
 function toDifficulty() {
+    document.getElementById("helpMenu").classList.add("hide")
+    document.getElementById("gameplay").classList.add("hide")
+    document.getElementById("quit").classList.add("hide")
+    document.getElementById("gameTitle").classList.remove("hide");
+    document.getElementById("difficultySelect").classList.remove("hide")
+}
 
+function toGameplay() {
+    document.getElementById("difficultySelect").classList.add("hide")
+    document.getElementById("helpMenu").classList.add("hide")
+    document.getElementById("gameTitle").classList.remove("hide")
+    document.getElementById("gameplay").classList.remove("hide")
+    document.getElementById("quit").classList.remove("hide")
 }
