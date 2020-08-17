1ac0257ed86181f74e09ac5313c7576ad066401d
danielxie <xied@macstudent.com.au>
2020-08-18 08:17:21 +1000

    working easy, hard and testing mode, switching screens

--- projects/wam/wam.js
+++ projects/wam/wam.js
@@ -6,8 +6,11 @@ const startButton = document.querySelector("#startButton")
 const moles = document.querySelectorAll(".mole")
 const scoreText = document.querySelector("#score")
 const remainingText = document.querySelector("#remaining")
+const finalScore = document.querySelector("#finalScore")
+const highScoreText = document.querySelector("#highScore")
 
 let score = 0
+let highScore = 0
 let counter = 10
 let prevNum = null
 let easyTimer = 1000
@@ -30,10 +33,10 @@ testButton.addEventListener("click", function() {
 })
 startButton.addEventListener("click", function() {
     startButton.disabled = true
-    if (difficulty === "easy" || difficulty ==="hard") {
+    if (difficulty === "easy" || difficulty === "hard") {
         clickSetUp()
         playGame()
-    } else if (difficulty ==="test") {
+    } else if (difficulty === "test") {
         playTest()
     }
 })
@@ -68,13 +71,13 @@ function playGame() {
         moles[prevNum].style.visibility="hidden"
     }
     if (counter < 1) {
-        return
+        loadFinish()
     }
     number = selectCell()
     prevNum = number
     moles[number].style.visibility="visible"
     remaining--
-    remainingText.innerText=remaining.toString()
+    remainingText.innerText = remaining.toString()
     counter -= 1
     if (difficulty === "easy") {
         setTimeout(playGame, easyTimer)
@@ -87,8 +90,8 @@ function playTest() {
     if (prevNum !== null) {
         moles[prevNum].style.visibility="hidden"
     }
-    if (counter <= 0 ) {
-        return
+    if (counter < 1) {
+        loadFinish()
     }
     number = selectCell()
     prevNum = number
@@ -102,16 +105,52 @@ function playTest() {
                 score++
                 scoreText.innerText = score.toString()
                 moles[number].style.visibility = "hidden"
-                console.log(counter, score, number)
                 playTest()
             }
         })
     }
 }
 
+function loadGame() {
+    document.getElementById("game").classList.remove("hide")
+    document.getElementById("game").classList.add("show")
+    document.getElementById("start").classList.remove("show")
+    document.getElementById("start").classList.add("hide")
+    document.getElementById("finish").classList.remove("show")
+    document.getElementById("finish").classList.add("hide")
 
-
+}
 function loadInstructions() {
     document.getElementById("instructions").classList.remove("hide")
     document.getElementById("instructions").classList.add("show")
+    document.getElementById("start").classList.remove("show")
+    document.getElementById("start").classList.add("hide")
+}
+function loadStart() {
+    document.getElementById("start").classList.remove("hide")
+    document.getElementById("start").classList.add("show")
+    document.getElementById("instructions").classList.remove("show")
+    document.getElementById("instructions").classList.add("hide")
+    document.getElementById("game").classList.remove("show")
+    document.getElementById("game").classList.add("hide")
+    document.getElementById("finish").classList.remove("show")
+    document.getElementById("finish").classList.add("hide")
+}
+function loadFinish() {
+    document.getElementById("finish").classList.remove("hide")
+    document.getElementById("finish").classList.add("show")
+    document.getElementById("game").classList.remove("show")
+    document.getElementById("game").classList.add("hide")
+    finalScore.innerHTML = score.toString()
+    if (score > highScore) {
+        highScore = score
+        highScoreText.innerText = "New High Score!"
+    } else if (score === highScore) {
+        highScoreText.innerText = "High Score!"
+    } else {
+        highScoreText.innerHTML = "Try to beat your high score of " + highScore.toString()
+    }
+}
+function resetGame() {
+
 }
