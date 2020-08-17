e83da8376e73ebf3a3880086b6be9790f5aae5a0
danielxie <xied@macstudent.com.au>
2020-08-17 20:43:34 +1000

    working easy and hard mode

--- projects/wam/wam.js
+++ projects/wam/wam.js
@@ -1,15 +1,21 @@
 const easyButton = document.querySelector("#easyButton")
 const hardButton = document.querySelector("#hardButton")
+const testButton = document.querySelector("#testButton")
 const difficultyText = document.querySelector("#difficulty")
 const startButton = document.querySelector("#startButton")
 const moles = document.querySelectorAll(".mole")
+const scoreText = document.querySelector("#score")
+const remainingText = document.querySelector("#remaining")
 
 let score = 0
 let counter = 10
-let prevNum = -1
+let prevNum = null
 let easyTimer = 1000
 let hardTimer = createHardTimer(750, 1000)
 let difficulty = null
+let number = null
+let remaining = 10
+let moleClicked = false
 
 easyButton.addEventListener("click", function() {
     difficultyText.innerHTML="Difficulty: <strong>Easy</strong>"
@@ -19,8 +25,16 @@ hardButton.addEventListener("click", function() {
     difficultyText.innerHTML="Difficulty: <strong>Hard</strong>"
     difficulty = "hard"
 })
+testButton.addEventListener("click", function() {
+    difficultyText.innerHTML="Difficulty: <strong>Test Mode</strong>"
+    difficulty = "test"
+})
 startButton.addEventListener("click", function() {
-    playGame()
+    if (difficulty === "easy" || difficulty ==="hard") {
+        playGame()
+    } else if (difficulty ==="test") {
+        playTest()
+    }
 })
 
 function createHardTimer(min, max) {
@@ -37,19 +51,47 @@ function selectCell() {
 }
 
 function playGame() {
-    if (prevNum !== -1) {
+    if (prevNum !== null) {
         moles[prevNum].style.visibility="hidden"
     }
     if (counter < 1) {
         return
     }
-    counter -= 1
-    let number = selectCell()
+    number = selectCell()
     prevNum = number
     moles[number].style.visibility="visible"
+    remaining--
+    remainingText.innerText=remaining.toString()
+    counter -= 1
     if (difficulty === "easy") {
         setTimeout(playGame, easyTimer)
-    } else {
+    } else if (difficulty === "hard") {
         setTimeout(playGame, hardTimer)
     }
+}
+
+function playTest() {
+    number = selectCell()
+    prevNum = number
+    moles[number].style.visibility="visible"
+    remaining--
+    remainingText.innerText=remaining.toString()
+
+}
+
+
+function loadInstructions() {
+    document.getElementById("instructions").classList.remove("hide")
+    document.getElementById("instructions").classList.add("show")
+}
+
+for (let i = 0; i < moles.length; i++) {
+    moles[i].addEventListener("click", function() {
+        if (i === number) {
+            score++
+            moleClicked = true
+            scoreText.innerText=score.toString()
+            moles[number].style.visibility="hidden"
+        }
+    })
 }
