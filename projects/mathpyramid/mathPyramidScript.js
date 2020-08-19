ca3778166156ba86f028b00d07dfa76c3d87836d
yea <yea@macstudent.com.au>
2020-08-19 10:06:46 +1000

    Set a structure of game in the testMP.html and add some function in the mathPyramidScript.js.

--- projects/mathpyramid/mathPyramidScript.js
+++ projects/mathpyramid/mathPyramidScript.js
@@ -3,6 +3,13 @@ var max = 0, difficulty = 0, L33 = 0, L32 = 0, L31 = 0, L21 = 0, L22 = 0, L11 = 
 function gameReady(n) {
     score = 0
     difficulty = n
+    if (difficulty = 1){
+        highestValue = 20
+    } else if(difficulty = 2){
+        highestValue = 100
+    }else {
+        highestValue = 500
+    }
 
 }
 
@@ -15,7 +22,7 @@ function toMenu(){
     game.style.display = "none";
 }
 
-function toGame(){
+function toGame(n){
     var help = document.getElementById("helpMP");
     var menu = document.getElementById("menuMP");
     var game = document.getElementById("gameMP");
@@ -31,4 +38,12 @@ function toHelp(){
     menu.style.display = "none";
     help.style.display = "inline-block";
     game.style.display = "none";
+}
+
+function nextGame(){
+
+}
+
+function reasult(){
+
 }
