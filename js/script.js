39ed2480accb941d2e7296d53700013d31da9a12
will <theawesomew@gmail.com>
2020-08-21 09:32:19 +1000

    Ability to get and log highscore data beginning to form

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
+let user;
+let apiURL = "https://sheetsu.com/apis/v1.0su/ff1114550f8e";
+
+function errFunc (e) { throw new Error(e) }
+function successFunc () { console.log("you idea is good and you should feel good :)") }
+
 function onSignIn (googleUser) {
     let profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
+
+    console.table([{attribute: 'Email', value: profile.getEmail()}]);
+
+    Sheetsu.read(apiURL, {search: {email: profile.getEmail()}})
+        .then(function (data) {
+            user = {...data[0]};
+            console.log(user);
+        }, errFunc);
 }
 
 function signOut() {
@@ -11,4 +21,8 @@ function signOut() {
     auth2.signOut().then(function () {
         console.log('User signed out.');
     });
+}
+
+function newHighScore (user, id, score) {
+
 }
