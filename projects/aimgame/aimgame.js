2401207ec93083abe7feb3494239b2ad2982fa00
Keeeean <suk@macstudent.com.au>
2020-08-21 14:36:22 +1000

    3rd commit : hid canvas, show buttons, when expert is clicked, canvas is shown.

--- projects/aimgame/aimgame.js
+++ projects/aimgame/aimgame.js
@@ -7,10 +7,16 @@ function difficultySelection() {
     document.getElementById("buttons").classList.add("show");
 }
 
+function hideDifficulty() {
+    document.getElementById("buttons").classList.remove("show");
+    document.getElementById("buttons").classList.add("hide");
+    document.getElementById("aimTitle").classList.add("hide");
 
-let targethit = 0;
-let targetamountHard = 0;
-document.getElementById("targetHit").textContent = targethit;
+}
+
+let targetHits = 0;
+let targetAmountHard = 0;
+document.getElementById("targetHit").textContent = targetHits;
 
 
 const player = { // making sure the the 1st target will always appear in the middle of the screen.
@@ -18,62 +24,50 @@ const player = { // making sure the the 1st target will always appear in the mid
     y: 490,
     colour: "cornflowerblue"
 }
-window.addEventListener("mousedown", onclickHard);
-var keys = []
+window.addEventListener("mousedown", onclick);
 
-
-window.addEventListener("keydown", function (e) {
-    keys[e.keyCode] = true;
-
-}, false);
-
-
-window.addEventListener("keyup", function (e) {
-    delete keys[e.keyCode];
-
-}, false);
-
-
-function onclickHard (e) {
+function onclick (e) {
     let el = e.target;
     let px = e.clientX - el.offsetLeft;
     let py = e.clientY - el.offsetTop;
     if ((player.x -px)**2 + (player.y - py)**2 < 50**2) {
         console.log("hit");
-        targethit = targethit + 1;
-        targetamountHard = targetamountHard + 1
-        document.getElementById("targetHit").textContent = targethit;
-        player.x = 1600 * Math.random();
-        player.y = 950 * Math.random();
+        targetHits = targetHits + 1;
+        targetAmountHard = targetAmountHard + 1
+        document.getElementById("targetHit").textContent = targetHits;
+        hardBoundary();
     }
     else {
         console.log("you're cringe.");
-        player.x = 1600 * Math.random();
-        player.y = 800 * Math.random();
-        targetamountHard = targetamountHard + 1
+        hardBoundary();
+        targetAmountHard = targetAmountHard + 1
     }
+
 
 
 } // function that checks whether or not the user clicks is within the target; hits the target.
 
-
+function hardBoundary(){
+    player.x = 1600 * Math.random();
+    player.y = 800 * Math.random();
+}
 
 function runHard () { // function that runs the target game.
     var loop = function () {
-        updateHard();
+        /*updateHard();*/
         render();
         window.requestAnimationFrame(loop);
 
     }
     window.requestAnimationFrame(loop);
 }
-function updateHard() {
+/*function updateHard() {
         if (keys[32]) {
             player.x = 1600 * Math.random();
             player.y = 950 * Math.random();
 
         }
-}
+}*/
 function render() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "cornflowerblue";
@@ -81,4 +75,3 @@ function render() {
     ctx.arc(player.x, player.y, 50, 0, 2 * Math.PI)
     ctx.fill();
 }
-
