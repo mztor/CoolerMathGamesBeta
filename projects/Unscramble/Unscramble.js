2c471a0edcf0f158fed693589269d4015edd250d
morganc1 <morganc1@macstudent.com.au>
2020-08-22 21:12:52 +1000

    made quit button refresh page to reset changed style of guess input

--- projects/Unscramble/Unscramble.js
+++ projects/Unscramble/Unscramble.js
@@ -1,18 +1,12 @@
-function toTitlePage() {
-    document.getElementById("helpMenu").classList.add("hide")
-    document.getElementById("gameplay").classList.add("hide")
-    document.getElementById("quit").classList.add("hide")
-    document.getElementById("difficultySelect").classList.add("hide")
-    document.getElementById("gameTitle").classList.remove("hide")
-    document.getElementById("titlePage").classList.remove("hide")
-}
+let rounds = 10;
+let wordIndex = 0;
+let score = 0;
 
 function toHelp() {
     document.getElementById("gameTitle").classList.add("hide");
     document.getElementById("difficultySelect").classList.add("hide");
     document.getElementById("helpMenu").classList.remove("hide")
 }
-
 
 function toDifficulty() {
     document.getElementById("titlePage").classList.add("hide")
@@ -38,20 +32,22 @@ function checkGuess(word, score) {
         score = score + 10
         document.getElementById("scoreTotal").textContent = score
     } else {
-        document.getElementById("s_word").textContent = word
         document.getElementById("mark").textContent = " ✗"
 
     }
-    document.getElementById("nextRound").classList.remove("hide")
+    document.getElementById("s_word").textContent = word
     document.getElementById("mark").classList.remove("hide")
     if (words_1.includes(word)) {
         document.getElementById("submit_1").classList.add("hide")
+        document.getElementById("nextRound_1").classList.remove("hide")
     }
     else if (words_2.includes(word)) {
         document.getElementById("submit_2").classList.add("hide")
+        document.getElementById("nextRound_2").classList.remove("hide")
     }
     else if (words_3.includes(word)) {
         document.getElementById("submit_3").classList.add("hide")
+        document.getElementById("nextRound_3").classList.remove("hide")
     }
 }
 
@@ -81,18 +77,9 @@ function shuffleWord(originalWord) {
     return shuffled;
 }
 
-words_1 = ["bug", "fix", "ice", "spy", "pie", "sky", "sun", "toy", "oil", "shy"]
+words_1 = ["bug", "fix", "ice", "spy", "pie", "new", "sun", "toy", "oil", "shy"]
 words_2 = ["eggs", "exit", "edge", "have", "then", "when", "here", "word", "area", "find"]
 words_3 = ["catch", "hedge", "light", "heavy", "where", "block", "guess", "lunch", "month", "round"]
-
-
-let rounds = 10;
-let wordIndex = 0;
-let score = 0
-// Main Program
-//    function startGame(difficulty) {
-//        score = 0o00
-//    }
 
 function resetOnQuit() {
     rounds = 10;
@@ -103,16 +90,18 @@ function resetOnQuit() {
 
 function getNextWord(difficulty) {
     if (rounds > 0) {
-        document.getElementById("nextRound").classList.add("hide")
         document.getElementById("mark").classList.add("hide")
         if (difficulty === "L1") {
             document.getElementById("submit_1").classList.remove("hide")
+            document.getElementById("nextRound_1").classList.add("hide")
         }
         else if (difficulty === "L2") {
             document.getElementById("submit_2").classList.remove("hide")
+            document.getElementById("nextRound_2").classList.add("hide")
         }
         else if (difficulty === "L3") {
             document.getElementById("submit_3").classList.remove("hide")
+            document.getElementById("nextRound_3").classList.add("hide")
         }
         document.getElementById("s_word").textContent = generateWord(difficulty, wordIndex)
         wordIndex = wordIndex + 1
@@ -121,6 +110,7 @@ function getNextWord(difficulty) {
     else {
         document.getElementById("gameplay").classList.add("hide")
         document.getElementById("nextRound").classList.add("hide")
+        document.getElementById("winScreen").classList.remove("hide")
         }
 }
 
