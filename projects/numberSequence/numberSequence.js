1c1feb731e2200b709f235477e73dc54813ba904
C-Schwigtenberg <schwigtenbergc@macstudent.com.au>
2020-08-23 15:13:25 +1000

    Added a form cover for the answer entry, as well as a drop down select for the operation selection. The JS file has been separated from the HTML one, and some functions have been written for checking answers and question loading.

--- projects/numberSequence/numberSequence.js
+++ projects/numberSequence/numberSequence.js
@@ -1,4 +1,6 @@
+var points = 0;
 function loadEasy() {
+	var question = 1;
     document.getElementById("difficulty_screen").classList.remove('buttonHolder');
     document.getElementById("difficulty_screen").classList.add('invisibleStuff');
     document.getElementById("questionScreen").classList.toggle("invisibleStuff");
@@ -10,7 +12,6 @@ function loadEasy() {
     }
 }
 function easyAddition() {
-    var question = 1;
     while (question < 6) {
         var base = Math.floor((Math.random() * 10) + 1);
         var number = Math.floor((Math.random() * 10) + 1);
@@ -21,11 +22,43 @@ function easyAddition() {
         document.getElementById("ThirdPlaceholder").textContent = number;
     }
 }
+
+function easySubtraction() {
+	while (question < 6) {
+		var base = Math.floor((Math.random() * 10) + 1);
+        var number = Math.floor((Math.random() * 10) + 1);
+        document.getElementById("FirstPlaceholder").textContent = number;
+        number = number - base;
+        document.getElementById("SecondPlaceholder").textContent = number;
+        number = number - base;
+        document.getElementById("ThirdPlaceholder").textContent = number;
+	}
+}
+
+function answerCheck() {
+	if (int(document.getElementById("SecondPlaceholder").textContent()) > int(document.getElementById("FirstPlaceholder").textcontent())) {
+		if (document.getElementById("operation").value = "addition") {
+			if (int(document.getElementById("answerField").value) == (int(document.getElementById("SecondPlaceholder").textContent()) - int(document.getElementById("FirstPlaceholder").textcontent())) {
+				points = points + 10;
+				document.getElementById("score").textContent = score;
+			}
+		}
+	} else if (int(document.getElementById("SecondPlaceholder").textContent()) < int(document.getElementById("FirstPlaceholder").textcontent())) {
+		if (document.getElementById("operation").value = "subtraction") {
+			if (int(document.getElementById("SecondPlaceholder")) + int(document.getElementById("answerField").value) == int(document.getElementById("FirstPlaceholder").textContent)){
+				points = points + 10;
+				document.getElementById("score").textContent = score;
+			}
+		}
+	}
+}
+
 function load_game2() {
     document.getElementById("difficulty_screen").classList.remove('buttonHolder');
     document.getElementById("difficulty_screen").classList.add('invisibleStuff');
     document.getElementById("questionScreen").classList.toggle("invisibleStuff");
 }
+
 function load_game3() {
     document.getElementById("difficulty_screen").classList.remove('buttonHolder');
     document.getElementById("difficulty_screen").classList.add('invisibleStuff');
