fa6c13c8a9a3893c502533682ff4004c9baa3fa3
thomastindal <tindalt@macstudent.com.au>
2020-08-24 08:57:22 +1000

    3rd commit: Updated the winstreak counter and also found out how to cycle through the game although I still need to find out how to reset the scores.

--- projects/rockpaperscissors/rockpaperscissors.js
+++ projects/rockpaperscissors/rockpaperscissors.js
@@ -66,18 +66,19 @@ function Check() {
     if (computerboard === 10) {
         document.getElementById("StartStandard").style.display = "none";
         document.getElementById("LoserEndScreen").style.display = "block";
+        winstreak = 0;
     }
     if (playerboard === 10) {
         document.getElementById("StartStandard").style.display = "none";
         document.getElementById("WinnerEndScreen").style.display = "block";
+        winstreak++;
     }
-
-
     document.getElementById("responses").style.display = "none";
     document.getElementById("button2").style.display = "block";
     document.getElementById("changethis").style.display = "block";
     document.getElementById("playerscore").style.display = "block";
     document.getElementById("computerscore").style.display = "block";
+    document.getElementById("winstreakchange").textContent = winstreak;
 }
 responses.onclick = Check;
 function startbutton() {
@@ -93,6 +94,7 @@ function standardmode2() {
     document.getElementById("responses").style.display = "block";
     document.getElementById("response").style.display = "block"
     document.getElementById("response2").style.display = "block"
+    document.getElementById("StartStandard").style.display = "block"
 }
 standardbutton.onclick = standardmode2;
 
@@ -100,27 +102,18 @@ function reset() {
     document.getElementById("responses").style.display = "block"
     document.getElementById("button2").style.display = "none"
     document.getElementById("changethis").style.display = "none"
-
     response2.textContent = "You";
     response.textContent = "Computer";
 }
 button2.onclick = reset;
 function restartgamefromloss() {
     document.getElementById("LoserEndScreen").style.display = "none"
-    computerboard = 0;
-    playerboard = 0;
-    winstreak = 0;
     document.getElementById("Startbutton").style.display = "block"
-
 }
 restartLoss.onclick = restartgamefromloss;
 
 function restartgamefromwin() {
     document.getElementById("WinnerEndScreen").style.display = "none"
-    computerboard = 0;
-    playerboard = 0;
-    winstreak++;
     document.getElementById("Startbutton").style.display = "block"
-
 }
 restartWin.onclick = restartgamefromwin;
