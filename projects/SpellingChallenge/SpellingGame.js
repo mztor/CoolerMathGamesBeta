8bb7754852bcec920dabaaa5e25496c7b7e1c951
zacharyfielder <fieldez@macstudent.com.au>
2020-08-25 09:44:44 +1000

    3rd Commit: Made user screen disappear and mapped difficulty buttons

--- projects/SpellingChallenge/SpellingGame.js
+++ projects/SpellingChallenge/SpellingGame.js
@@ -1,19 +1,43 @@
-function ToDifficulty () {
+var kindywordsarray = [
+    { name: 'Bat', image: 'Bat.png' },
+    { name: 'Cat', image: 'Cat.png' },
+    { name: 'Hat', image: 'Hat.jpg' },
+    { name: 'Sea', image: 'Sea.jpg' },
+    { name: 'Sun', image: 'Sun.png' }
+];
+var Year1wordsarray = [
+    { name: 'plum', image: 'plum.png' },
+    { name: 'Tiger', image: 'Tiger.png' },
+    { name: 'Helmet', image: 'Helmet.jpg' },
+    { name: 'Ocean', image: 'Ocean.jpg' },
+    { name: 'Venus', image: 'Venus.png' }
+];
+var Year2wordsarray = [
+    { name: 'possum', image: 'possum.png' },
+    { name: 'Leopard', image: 'Leopard.png' },
+    { name: 'Cowboy', image: 'Cowboy.jpg' },
+    { name: 'Estuary', image: 'Sea.jpg' },
+    { name: 'Jupiter', image: 'Jupiter.png' }
+];
+function ClearUser(){
+    // alert("cs")
+    document.getElementById('UserScreen').style = 'display:none;'
+    document.getElementById('DifficultySelectionScreen').style = 'display:block';
+}
+
+function Loadkindy(kindy){
+alert(kindy)
 
 }
 
-function Startkindy () {
-
+function LoadYear1 (Year1) {
+    alert(Year1)
 }
 
-function StartYear1 () {
-
+function LoadYear2 (Year2) {
+    alert(Year2)
 }
 
-function StartYear2 () {
-
-}
-
-function StartHelp () {
-
+function LoadHelp (Help) {
+    alert(Help)
 }
